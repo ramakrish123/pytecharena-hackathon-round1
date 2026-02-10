fa97308b41eeb57ecfb0c0422897c62339afea66
ramakrishna <143248712+ramakrish123@users.noreply.github.com>
2026-02-10 15:27:47 +0530

    solved q1

--- q1.py
+++ q1.py
@@ -4,4 +4,14 @@ def transform_string(s: str) -> str:
     Consonants → lowercase
     Digits & special chars unchanged
     """
-    pass
+        result = []
+    for ch in s:
+        if ch.isalpha():
+            if ch.lower() in "aeiou":
+                result.append(ch.upper())
+            else:
+                result.append(ch.lower())
+        else:
+            result.append(ch)
+    return "".join(result)
+
