0457c1d8e0b0e983528e0c7e50531b9b57b58c97
Vinay Juttuka <147337163+Juttukavinay@users.noreply.github.com>
2026-02-10 14:23:03 +0530

    List Analyzer
    Define a function to analyze a list of numbers.

---
+++ q2.py
@@ -0,0 +1,11 @@
+def analyze_list(numbers: list) -> dict:
+    """
+    Returns:
+    {
+      'sum': int,
+      'mean': float,
+      'even_count': int,
+      'odd_count': int
+    }
+    """
+    pass
